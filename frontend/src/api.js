e26477358e5b9a5f57ea7bd440e6ce6f339401ce
Nikhil Gupta <ng4303020@gmail.com>
2026-05-05 20:34:23 +0530

    updated api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://ultimate-task-manager-production.up.railway.app/api"
 });
 
 // 🔐 Attach auth headers automatically
