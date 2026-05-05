cbe0b0ca49ec4d600c1d66e2ec057d9d5d9c7c60
Nikhil Gupta <ng4303020@gmail.com>
2026-05-05 21:50:57 +0530

    fix allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   preview: {
     host: true,
     port: process.env.PORT,
     allowedHosts: "all"
   }
-}
+})
